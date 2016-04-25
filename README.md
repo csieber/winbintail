57b2cd836a5b62485f4a88862a3256ad9a68af27
Christian Sieber <c.sieber@tum.de>
2016-04-25 11:53:12 -0400

    Added source and cmake file. Updated README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # winbintail
+
 Simple cross-platform binary tail command.
+
+## QUICKSTART
+
+Clone the source:
+
+```
+git clone git@github.com:csieber/winbintail.git
+cd winbintail
+```
+
+Compile the application using cmake:
+
+```
+mkdir bin
+cd bin
+cmake ../
+make
+```
+
+Start it with the file to monitor as the first argument:
+
+```
+cd bin/bin
+./winbintail abinaryfile.bin
+```
+
---
+++ src/winbintail.cpp
@@ -0,0 +1,70 @@
+//============================================================================
+// Name        : Windows Bin Tail
+// Author      : Christian Sieber <sieber@informatik.uni-wuerzburg.de>
+// Version     :
+// Copyright   : Christian Sieber
+// Description : Simple cross-platform binary tail command.
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <inttypes.h>
+
+#ifdef _WIN32
+	#include <windows.h>
+	#include <fcntl.h> // _O_BINARY
+	unsigned int _CRT_fmode = _O_BINARY;
+
+#else
+	#include <unistd.h>
+#endif
+
+/*
+ * Settings
+ */
+const int32_t poll_interval_millis = 100;
+const int32_t buff_size = 500000;
+
+int main(int argc, const char** argv) {
+
+	if (argc == 1) {
+
+		std::cerr << "Usage: winbintail [file]" << std::endl;
+		return 1;
+	}
+
+	std::ifstream file;
+
+	file.open(argv[1], std::ifstream::in | std::ifstream::binary);
+
+	if (file.fail()) {
+
+		std::cerr << "Failed to open file!" << std::endl;
+		return 1;
+	}
+
+	char buff[buff_size];
+	std::streamsize read;
+
+	while (!file.fail()) {
+
+		read = file.readsome(buff, buff_size);
+
+		if (read > 0) {
+
+			std::cout.write(buff, read);
+			std::cout.flush();
+			continue;
+		}
+
+#ifdef _WIN32
+		Sleep(poll_interval_millis);
+#else
+		usleep(poll_interval_millis*1000);
+#endif
+	}
+
+	file.close();
+
+	return 0;
+}
